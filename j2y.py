4f83a401ea35d4da0eacbeb9c23bf5c95cff033d
Joey8419 <jnelson297@gmail.com>
2023-11-17 22:43:59 -0500

    finished json yaml converter

---
+++ j2y.py
@@ -0,0 +1,14 @@
+import json
+import yaml
+def converter(file1):
+    with open(file1, 'r') as f:
+        new_file1 = json.load(f)
+
+    with open(file1.replace(".json", ".yaml"), "w") as yaml_file:
+        yaml.dump(new_file1, yaml_file)
+
+    with open(file1, 'r') as yaml_file:
+        print(yaml_file.read())
+
+converter("donuts.json")
+converter("emojis.json")
